ca072925ec8466a3a67524b3d6dfb5f2791cff58
O.N.B <onbh4ck3r@gmail.com>
2018-03-02 16:55:54 +0100

    Add files via upload

---
+++ ordenadores/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name':'Venta Ordenadores',
+    'sumary': 'Tienda Venta de ordenadores',
+    'author': 'Burguer',
+    'version': '2.3.1',
+    'website': 'www.informatic.com',
+    'summary': 'Esta es una aplicacion diseñada para el optimo funcionamiento de una tienda de ordenadores',
+    'depends': ['base'],
+    'data':['views/ordenador.xml','views/ofertas.xml','views/proveedores.xml', 'views/tipos.xml'],
+    'images': [
+        'static/description/icon.png',
+    ],
+}
